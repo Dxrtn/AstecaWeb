41ec1601d76e95aec350932749197a0ab326f61d
Davi Duarte <davi@astecaweb.com.br>
2026-04-23 00:14:57 -0300

    Node e Express inicializados e estrutura montada, faltando apenas as partials

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express"
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+
+    res.render('index')
+})
+
+app.get('/login', (req, res) => {
+
+    res.render('login')
+})
+
+app.get('/cadastro', (req, res) => {
+
+    res.render('cadastro')
+})
+
+
+app.listen(3000)
