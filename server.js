2080c257fee4f829a20b794a0aefc82f2c24f700
Davi Duarte <davi@astecaweb.com.br>
2026-04-23 08:55:57 -0300

    Corrigindo erro de servidor do Vercel

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('views', './views')
 
 app.get('/', (req, res) => {
